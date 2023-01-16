af149f5a04bf35526bbdfffc785918ef1a2a96eb
Dead_Skater666 <kristina192003@gmail.com>
2023-01-16 13:43:14 +0600

    .

---
+++ src/components/Error/Error.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Cerber from "../storage/error404.jpg"
+const Error = () => {
+    return (
+        <div>
+            <img src={Cerber} alt="" />
+        </div>
+    );
+};
+
+export default Error;
