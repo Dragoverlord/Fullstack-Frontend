8020ec61f1d19a804cb2ec88d62cd2e3f5bf8ce7
unknown <kamcybekov26@gmail.com>
2023-01-25 20:02:19 +0600

    style

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,17 +1,17 @@
-import React from 'react';
+import React from "react";
 import { ChatEngine } from "react-chat-engine";
-import NavbarCustom from '../navbar/Navbar';
+import NavbarCustom from "../navbar/Navbar";
 const Chat = () => {
-    return (
-        <>
-        <NavbarCustom/>
-           <ChatEngine
-            projectID="4031ffd2-1596-4f7f-9fb3-523f1c8b7189"
-            userName="User"
-            userSecret="User"
-          />
-        </>
-    );
+  return (
+    <>
+      <NavbarCustom />
+      <ChatEngine
+        projectID="4031ffd2-1596-4f7f-9fb3-523f1c8b7189"
+        userName="Albert"
+        userSecret="123"
+      />
+    </>
+  );
 };
 
 export default Chat;
